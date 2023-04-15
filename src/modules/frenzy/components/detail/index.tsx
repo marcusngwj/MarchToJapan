3c2141e90b53b1000767b57841a5610a6bee13ce
Marcus Ng <marcusngwj@msn.com>
2023-04-15 11:43:35 +0800

    fix overflow due to multiple tags

--- src/modules/frenzy/components/detail/index.tsx
+++ src/modules/frenzy/components/detail/index.tsx
@@ -17,15 +17,15 @@ export default function Index({ content }: DetailProp) {
         <Image src={content.src} name={content.name} />
       )}
 
-      <Box sx={{ fontWeight: "bold" }}>{content.name}</Box>
-      <Box sx={{ mt: 1 }}>{content.description}</Box>
-
-      <Box sx={{ display: "flex", mt: 3 }}>
-        {content.tags?.map((tag) => (
-          <Box sx={{ mr: 1 }}>#{tag}</Box>
-        ))}
+      <Box sx={{ p: 1 }}>
+        <Box sx={{ fontWeight: "bold" }}>{content.name}</Box>
+        <Box sx={{ mt: 1 }}>{content.description}</Box>
+        <Box sx={{ display: "flex", flexWrap: "wrap", mt: 3 }}>
+          {content.tags?.map((tag) => (
+            <Box sx={{ mr: 1 }}>#{tag}</Box>
+          ))}
+        </Box>
       </Box>
-
       <Box sx={{ display: "flex", justifyContent: "center", my: 3 }}>
         <Box sx={{ width: "75vw" }}>
           <ShareButton />
